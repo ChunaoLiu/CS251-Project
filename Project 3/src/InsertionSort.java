0cc2cb78bafcf43e02b6815d3b5d584faff30e13
Chunao Liu <787811283@qq.com>
2021-07-14 09:36:14 -0400

    Insertion EZ

--- Project 3/src/InsertionSort.java
+++ Project 3/src/InsertionSort.java
@@ -16,6 +16,15 @@ public class InsertionSort {
      */
     public static int insertionSort(SortArray array) {
         //TODO: Implement insertionSort
+        for (int i = 0; i < array.length(); i++) {
+            for (int j = i + 1; j < array.length(); j++) {
+                if (array.get(i) > array.get(j)) {
+                    int temp = array.get(i);
+                    array.set(i, array.get(j));
+                    array.set(j, temp);
+                }
+            }
+        }
 
         return arrayAccesses;
     }
--- Project 3/src/Merge.java
+++ Project 3/src/Merge.java
@@ -10,6 +10,25 @@ public class Merge {
      */
     public static int sort(SortArray array) {
         //TODO implement mergeSort
+        if (array.length() == 1) {
+            return array.get(0);
+        }
+        int midpoint = 1 + array.length() / 2;
+        int leftArr[] = new int[midpoint];
+
+        int rightArr[] = new int[array.length() - midpoint];
+
+        for (int i = 0; i < midpoint; i++) {
+            leftArr[i] = array.get(i);
+        }
+        for (int i = midpoint; i < array.length(); i++) {
+            rightArr[i] = array.get(i);
+        }
+
+        SortArray left = new SortArray(leftArr);
+        SortArray right = new SortArray(rightArr);
+        
+        sort(left)
         return arrayAccesses;
     }
 
